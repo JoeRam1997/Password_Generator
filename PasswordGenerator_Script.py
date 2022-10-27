67737c2f4f2260025ba2727866ac4b3722697bf2
JoeRam1997 <joseph.ramirez10@okstate.edu>
2022-10-26 19:24:39 -0500

    Password Requirements

--- PasswordGenerator_Script.py
+++ PasswordGenerator_Script.py
@@ -2,6 +2,19 @@
 import random
 import string
 
+# Welcome Statement
+print("Hola, Welcome to your Password Generator!")
+
+# Input the length of the password
+length = int(input('\nEnter the length needed for the password please:'))
+
+# Define password requirements
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+num = string.digits
+symbols = string.punctuation
+
+
 
 
 
